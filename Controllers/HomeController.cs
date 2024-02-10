671fe0c9f3ee0f22bf5e363ee85b4799e65ddd85
narikang <narikang333@gmail.com>
2024-02-09 20:18:39 -0700

    Added an icon, changed header and title

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,7 @@ namespace NariKang.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+      
+
     }
 }
---
+++ wwwroot/js/tutoringCalculator.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = $('#hours').val();
+        var rate = $('#rate').val();
+        if (hours > 0) {
+            var total = hours * rate;
+            $('#total').val(total.toFixed(2));
+        } else {
+            alert('Please enter a positive number of hours.');
+            $('#hours').val('');
+            $('#total').val('');
+        }
+    });
+});
